cbeed05d3bce499bb73ec03a63508bd8c5b916cf
drinfernoo <drinfernoo@yahoo.com>
2020-02-03 17:06:33 -0800

    :zap: - Adjust timing and remove setting for it

--- plugin.program.autowidget/service.py
+++ plugin.program.autowidget/service.py
@@ -1,6 +1,7 @@
 import xbmc
 import xbmcaddon
 
+import random
 import time
 
 from resources.lib import path_utils
@@ -8,7 +9,6 @@ from resources.lib.common import utils
 
 _addon = xbmcaddon.Addon()
 _monitor = xbmc.Monitor()
-_wait_time = int(_addon.getSetting('service.wait_time'))
 
 utils.log('+++++ STARTING AUTOWIDGET SERVICE +++++', level=xbmc.LOGNOTICE)
 utils.log('+++++ REFRESHING EVERY {} HOURS... +++++'.format(_wait_time), level=xbmc.LOGNOTICE)
@@ -16,8 +16,9 @@ utils.log('+++++ REFRESHING EVERY {} HOURS... +++++'.format(_wait_time), level=x
 path_utils.refresh_paths()
 
 while not _monitor.abortRequested():
+    sleep_mins = 45 + int(random.random() * 30)
     try:
-        if _monitor.waitForAbort(_wait_time * 60 * 60):
+        if _monitor.waitForAbort(sleep_mins * 60):
             break
             
         path_utils.refresh_paths(notify=True)
