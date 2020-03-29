88db70932118b3da03d002acf6f0d5dd2bf2c4b9
drinfernoo <drinfernoo@yahoo.com>
2020-03-29 13:36:28 -0700

    :sparkles: - Improve toggles for service and notification
    Close #2

--- plugin.program.autowidget/service.py
+++ plugin.program.autowidget/service.py
@@ -8,7 +8,7 @@ from resources.lib import convert
 from resources.lib.common import utils
 
 _properties = ['context.autowidget']
-
+_player = xbmc.Player()
 
 class AutoWidgetService(xbmc.Monitor):
 
@@ -21,17 +21,18 @@ class AutoWidgetService(xbmc.Monitor):
         self._update_properties()
 
     def _reload_settings(self):
-        self.refresh_enabled = self._addon.getSettingBool('service.refresh_enabled')
+        self.refresh_enabled = self._addon.getSettingInt('service.refresh_enabled')
         self.refresh_duration = self._addon.getSettingNumber('service.refresh_duration')
-        self.refresh_notification = self._addon.getSettingBool('service.refresh_notification')
+        self.refresh_notification = self._addon.getSettingInt('service.refresh_notification')
 
     def _update_properties(self, window=10000):
         self._addon = xbmcaddon.Addon()
 
         for property in _properties:
-            utils.log('{}: {}'.format(property, xbmcaddon.Addon().getSetting(property)))
-            if self._addon.getSetting(property) == 'true':
-                xbmcgui.Window(window).setProperty(property, 'true')
+            setting = self._addon.getSetting(property)
+            utils.log('{}: {}'.format(property, setting))
+            if setting is not None:
+                xbmcgui.Window(window).setProperty(property, setting)
                 utils.log('Property {0} set'.format(property))
             else:
                 xbmcgui.Window(window).clearProperty(property)
@@ -46,10 +47,23 @@ class AutoWidgetService(xbmc.Monitor):
                 if self.waitForAbort(delay * self.refresh_duration):
                     break
 
-                if self.refresh_enabled:
-                    convert.refresh_paths(notify=self.refresh_notification)
+                if self.refresh_enabled in [0, 1]:
+                    notification = False
+                    if self.refresh_enabled == 1:
+                        if _player.isPlayingVideo():
+                            utils.log('+++++ PLAYBACK DETECTED, SKIPPING AUTOWIDGET REFRESH +++++', level=xbmc.LOGNOTICE)
+                            continue
+                    else:
+                        if self.refresh_notification == 0:
+                            notification = True
+                        elif self.refresh_notification == 1:
+                            if not _player.isPlayingVideo():
+                                notification = True
+                    
+                    utils.log('+++++ REFRESHING AUTOWIDGETS +++++', level=xbmc.LOGNOTICE)
+                    convert.refresh_paths(notify=notification)
                 else:
-                    utils.log('+++++ AUTOWIDGET SERVICE NOT ENABLED +++++', level=xbmc.LOGNOTICE)
+                    utils.log('+++++ AUTOWIDGET REFRESHING NOT ENABLED +++++', level=xbmc.LOGNOTICE)
         except Exception as e:
             utils.log(e, level=xbmc.LOGERROR)
 
