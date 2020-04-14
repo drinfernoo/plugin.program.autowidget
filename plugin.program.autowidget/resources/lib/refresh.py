b9113327b94f016b22f87edbedf2761289e747b0
drinfernoo <drinfernoo@yahoo.com>
2020-04-14 08:56:58 -0700

    :recycle: - Cleanup refresh process

--- plugin.program.autowidget/resources/lib/refresh.py
+++ plugin.program.autowidget/resources/lib/refresh.py
@@ -2,48 +2,17 @@ import xbmc
 import xbmcaddon
 import xbmcgui
 
-import ast
 import os
 import random
-import re
 import time
-import uuid
-
-from xml.etree import ElementTree
-
-try:
-    from urllib.parse import parse_qsl
-except ImportError:
-    from urlparse import parse_qsl
 
 from resources.lib import convert
 from resources.lib import manage
 from resources.lib.common import utils
 
 _addon = xbmcaddon.Addon()
-_addon_path = xbmc.translatePath(_addon.getAddonInfo('profile'))
-_addon_version = _addon.getAddonInfo('version')
 
-if xbmc.getCondVisibility('System.HasAddon(script.skinshortcuts)'):
-    _shortcuts = xbmcaddon.Addon('script.skinshortcuts')
-    _shortcuts_path = xbmc.translatePath(_shortcuts.getAddonInfo('profile'))
-else:
-    _shortcuts_path = ''
-_skin_root = xbmc.translatePath('special://skin/')
-_skin_id = os.path.basename(os.path.normpath(_skin_root))
-_skin = xbmcaddon.Addon(_skin_id)
-_skin_path = xbmc.translatePath(_skin.getAddonInfo('profile'))
-
-activate_window_pattern = '(\w+)*\((\w+\)*),*(.*?\)*),*(return)*\)'
 skin_string_pattern = 'autowidget-{}-{}'
-skin_string_info_pattern = '$INFO[Skin.String({})]'.format(skin_string_pattern)
-path_replace_pattern = '{}({})'
-widget_param_pattern = '^(?:\w+)(\W\w+)?$'
-uuid_pattern = ('\(([0-9a-fA-F]{8}'
-                '\-[0-9a-fA-F]{4}'
-                '\-[0-9a-fA-F]{4}'
-                '\-[0-9a-fA-F]{4}'
-                '\-[0-9a-fA-F]{12})\)$')
 
 
 def _get_random_paths(group_id, force=False, change_sec=3600):
@@ -99,29 +68,30 @@ def refresh(widget_id, widget_def=None, paths=None, force=False, duration=0):
         path_def = {}
         _id = widget_def['id']
         group_id = widget_def['group']
-        action = widget_def['action'].lower()
+        action = widget_def.get('action')
         setting = widget_def.get('setting')
         label_setting = widget_def.get('label_setting')
         current = widget_def.get('current')
-
-        if action == 'random' and len(paths) == 0:
-            paths = _get_random_paths(group_id, force)
-
-        if action == 'next':
-            paths = manage.find_defined_paths(group_id)
-            next = (current + 1) % len(paths)
-            path_def = paths[next]
-            widget_def['current'] = next
-        elif action == 'random':
-            path_def = paths.pop()
         
-        widget_def['path'] = path_def['id']
-        widget_def['updated'] = current_time
+        if action:
+            if action == 'random' and len(paths) == 0:
+                paths = _get_random_paths(group_id, force)
+
+            if action == 'next':
+                paths = manage.find_defined_paths(group_id)
+                next = (current + 1) % len(paths)
+                path_def = paths[next]
+                widget_def['current'] = next
+            elif action == 'random':
+                path_def = paths.pop()
             
-        convert.save_path_details(widget_def, _id)
-        _update_strings(_id, path_def, setting, label_setting)
-        
-        xbmc.executebuiltin('Container.Refresh()')
+            widget_def['path'] = path_def['id']
+            widget_def['updated'] = current_time
+                
+            convert.save_path_details(widget_def, _id)
+            _update_strings(_id, path_def, setting, label_setting)
+            
+            xbmc.executebuiltin('Container.Refresh()')
     
     return paths
 
