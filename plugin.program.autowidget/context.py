b97ae8d26afce9245be4cea5d083695f704d7722
drinfernoo <drinfernoo@yahoo.com>
2021-01-01 08:14:15 -0800

    :sparkles: - Add "Force Refresh Widgets" to context menu

--- plugin.program.autowidget/context.py
+++
@@ -1,9 +0,0 @@
-from resources.lib import add
-from resources.lib.common import utils
-
-
-if __name__ == '__main__':
-    utils.ensure_addon_data()
-
-    labels = add.build_labels('context')
-    add.add(labels)
---
+++ plugin.program.autowidget/context_add.py
@@ -0,0 +1,9 @@
+from resources.lib import add
+from resources.lib.common import utils
+
+
+if __name__ == '__main__':
+    utils.ensure_addon_data()
+
+    labels = add.build_labels('context')
+    add.add(labels)
