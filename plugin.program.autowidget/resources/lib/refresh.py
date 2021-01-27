ba5c2ac8bb1b1ecded7e272e8ff2324210641bc2
Dylan Jay <software@pretaweb.com>
2021-01-28 00:17:32 +0700

    return dummy files on error and update in the background. Notify when background updating

--- plugin.program.autowidget/resources/lib/refresh.py
+++ plugin.program.autowidget/resources/lib/refresh.py
@@ -327,6 +327,7 @@ def cache_and_update(widget_ids):
     or is expired and if so force it to be refreshed. When going through the queue this
     could mean we refresh paths that other widgets also use. These will then be skipped.
     """
+    dialog = xbmcgui.Dialog()
 
     assert widget_ids
     effected_widgets = set()
@@ -352,6 +353,7 @@ def cache_and_update(widget_ids):
             effected_widgets = effected_widgets.union(utils.widgets_for_path(path))
             if utils.is_cache_queue(hash):
                 # we need to update this path regardless
+                dialog.notification('AutoWidget', "Updating widget {}".format(_label))
                 new_files, files_changed = utils.cache_files(path, widget_id)
                 changed = changed or files_changed
                 utils.remove_cache_queue(hash)
