b2bbab6ce5d602d7f09722e1d98a7df7514d7be4
Dylan Jay <software@pretaweb.com>
2021-02-11 02:42:15 +0700

    copying instead of exploding

--- plugin.program.autowidget/resources/lib/add.py
+++ plugin.program.autowidget/resources/lib/add.py
@@ -268,7 +268,8 @@ def _copy_path(path_def):
         return
 
     progress = xbmcgui.DialogProgressBG()
-    progress.create(u"Exploding")
+    progress.create(u"Copying")
+    progress.update(1, u"Retrieving")
         
     group_def = manage.get_group_by_id(group_id)
     files, hash = refresh.get_files_list(path_def["file"]["file"], background=False)
