a6f1c65a64b128d43500b31099e3368d4ab8ebdb
Dylan Jay <software@pretaweb.com>
2021-02-22 14:46:53 +0700

    reduce wait for queue processing to 1s.

--- plugin.program.autowidget/resources/lib/menu.py
+++ plugin.program.autowidget/resources/lib/menu.py
@@ -379,6 +379,7 @@ def show_path(
                 "target": "next",
             }
 
+            next_path = file['file'] if not paged_widgets or merged else None
             directory.add_menu_item(
                 title=label,
                 params=update_params if paged_widgets and not merged else None,
@@ -388,6 +389,8 @@ def show_path(
                 isFolder=not paged_widgets or merged,
                 props=properties,
             )
+            # Ensure we precache next page for faster access
+            utils.push_cache_queue(utils.path2hash(next_path))
         else:
             filetype = file.get("type", "")
             title = {
